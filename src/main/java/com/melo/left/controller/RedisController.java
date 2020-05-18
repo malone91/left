332d2656ebe78f49d9e656836fd26db3aec46998
malong <760098065@qq.com>
2020-05-18 11:44:28 +0800

    dynamic redis param

--- src/main/java/com/melo/left/controller/RedisController.java
+++ src/main/java/com/melo/left/controller/RedisController.java
@@ -2,9 +2,7 @@ package com.melo.left.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("redis")
@@ -13,15 +11,15 @@ public class RedisController {
     @Autowired
     private RedisTemplate redisTemplate;
 
-    @GetMapping("set")
-    public String set() {
-        redisTemplate.opsForValue().set("melo", "haha");
+    @PostMapping("set")
+    public String set(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
         return "success";
     }
 
     @GetMapping("get")
-    public String get() {
-        Object value = redisTemplate.opsForValue().get("melo");
+    public String get(@RequestParam("key") String key) {
+        Object value = redisTemplate.opsForValue().get(key);
         return (String)value;
     }
 }
