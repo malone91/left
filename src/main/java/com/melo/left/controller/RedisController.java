e35ec813a57b027db407aea43822aaec21697dbb
malong <760098065@qq.com>
2020-05-22 19:50:49 +0800

    serialize mode

--- src/main/java/com/melo/left/controller/RedisController.java
+++ src/main/java/com/melo/left/controller/RedisController.java
@@ -1,12 +1,21 @@
 package com.melo.left.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.melo.left.model.ProgramLanguage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("redis")
 public class RedisController {
+
+    private static final TypeReference<List<ProgramLanguage>> TYPE_REFERENCE = new TypeReference<List<ProgramLanguage>>(){};
 
     @Autowired
     private RedisTemplate redisTemplate;
@@ -21,5 +30,27 @@ public class RedisController {
     public String get(@RequestParam("key") String key) {
         Object value = redisTemplate.opsForValue().get(key);
         return (String)value;
+    }
+
+    @PostMapping("set/list")
+    public String setList(String key) {
+        List<ProgramLanguage> values = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            ProgramLanguage language = new ProgramLanguage();
+            language.setId(1);
+            language.setType("Java" + i);
+            language.setDesc("Love" + i);
+            values.add(language);
+        }
+        redisTemplate.opsForValue().set(key, values);
+        log.info("serializer is {}", redisTemplate.getValueSerializer());
+        return "success";
+    }
+
+    @GetMapping("get/list")
+    public List<ProgramLanguage> getList(@RequestParam("key") String key) {
+        Object value = redisTemplate.opsForValue().get(key);
+        log.info("serializer is {}", redisTemplate.getValueSerializer());
+        return (List<ProgramLanguage>)value;
     }
 }
---
+++ src/main/java/com/melo/left/model/ProgramLanguage.java
@@ -0,0 +1,11 @@
+package com.melo.left.model;
+
+import lombok.Data;
+
+@Data
+public class ProgramLanguage {
+
+    private Integer id;
+    private String type;
+    private String desc;
+}
