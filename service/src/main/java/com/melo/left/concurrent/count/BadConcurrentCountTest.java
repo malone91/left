92101e45a6463b21dc10dc736239423c983f7afe
malong <760098065@qq.com>
2020-06-13 17:30:22 +0800

    add comment

--- service/src/main/java/com/melo/left/concurrent/count/BadConcurrentCountTest.java
+++ service/src/main/java/com/melo/left/concurrent/count/BadConcurrentCountTest.java
@@ -1,5 +1,12 @@
 package com.melo.left.concurrent.count;
 
+/**
+ * 如上面代码中的count += 1，
+ * 至少需要三条 CPU 指令。
+ * 指令 1：首先，需要把变量 count 从内存加载到 CPU 的寄存器；
+ * 指令 2：之后，在寄存器中执行 +1 操作；
+ * 指令 3：最后，将结果写入内存（缓存机制导致可能写入的是 CPU 缓存而不是内存）。
+ */
 public class BadConcurrentCountTest {
 
     private long count = 0;
