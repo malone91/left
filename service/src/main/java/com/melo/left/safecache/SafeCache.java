4cd3aaae7bd5cc7fee349d2c7528fd24c4666c76
malong <760098065@qq.com>
2020-06-21 14:36:27 +0800

    add comment for review

--- service/src/main/java/com/melo/left/safecache/SafeCache.java
+++ service/src/main/java/com/melo/left/safecache/SafeCache.java
@@ -7,7 +7,19 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
+ * 来自GeekTime Chapter 17 of Concurrent Program Practice
  * HashMap不是线程安全的，使用读写锁保证其线程安全
+ * ReadWriteLock不支持锁的升级，读锁没有释放时，获取写锁，会导致写锁永久等待，最终导致相关的线程阻塞，
+ * 永远没有机会被唤醒。所以不允许读写锁的升级。
+ * 但是锁的降级是允许的
+ *
+ * 线上系统停止响应了，CPU 利用率很低：读锁升级写锁导致阻塞？
+ * 代码查找：线上是Web应用，应用服务器比如说是Tomcat，并且开启了JMX，则可以通过JConsole等工具远程查看下线上死锁的具体情况
+ *
+ * ReentrantLock只有写锁支持条件变量，读锁不支持条件变量
+ *
+ * 不足：没有保证同步，即缓存和源头数据的一致性
+ * 最简单的解决方案：超时机制，数据库主动改动缓存
  * @param <K>
  * @param <V>
  */
