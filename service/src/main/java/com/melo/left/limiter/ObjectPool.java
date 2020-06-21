87044b0992148fd5bcc0fbc2f3ee60068e094139
malong-ds <760098065@qq.com>
2020-06-21 12:08:38 +0800

    fix

---
+++ service/src/main/java/com/melo/left/limiter/ObjectPool.java
@@ -0,0 +1,34 @@
+package com.melo.left.limiter;
+
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.Semaphore;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class ObjectPool<T, R> {
+
+    final List<T> pool;
+    final Semaphore semaphore;
+
+    public ObjectPool(T[] array) {
+        pool = new Vector<>();
+        int size = array.length;
+        IntStream.rangeClosed(0, size).forEach(e -> {
+            pool.add(array[e]);
+        });
+        semaphore = new Semaphore(size);
+    }
+
+    R exec(Function<T, R> function) throws InterruptedException {
+        T t = null;
+        semaphore.acquire();
+        try {
+            t = pool.remove(0);
+            return function.apply(t);
+        } finally {
+            pool.add(t);
+            semaphore.release();
+        }
+    }
+}
---
+++ service/src/main/java/com/melo/left/limiter/Test.java
@@ -0,0 +1,30 @@
+package com.melo.left.limiter;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String[] message = new String[11];
+        for (int i = 0; i < message.length; i++) {
+            message[i] = "obj_" + i;
+        }
+        ObjectPool<String, String> pool = new ObjectPool<>(message);
+        for (int i = 0; i < 100; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    pool.exec(t -> {
+                        System.out.println(Thread.currentThread().getId() + "当前对象T：" + t);
+                        return t;
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            thread.start();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
