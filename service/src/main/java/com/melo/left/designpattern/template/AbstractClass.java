071871df2bac5a3fa58d16d0c0d07ccfa1cfb5b7
malong <760098065@qq.com>
2020-07-17 19:43:06 +0800

    add comment

--- service/src/main/java/com/melo/left/designpattern/template/AbstractClass.java
+++ service/src/main/java/com/melo/left/designpattern/template/AbstractClass.java
@@ -2,6 +2,9 @@ package com.melo.left.designpattern.template;
 
 public abstract class AbstractClass {
 
+    /**
+     * sub class can't override because of final description
+     */
     public final void templateMethod() {
         System.out.println("父类方法开始");
         method1();
@@ -9,6 +12,10 @@ public abstract class AbstractClass {
         System.out.println("父类方法结束");
     }
 
+
+    /**
+     * sub class must override because of abstract description
+     */
     protected abstract void method1();
 
     protected abstract void method2();
