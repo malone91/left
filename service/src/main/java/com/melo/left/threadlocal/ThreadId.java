86e1787f945253ef8a6f1936306fd1eaec413c4d
malone91 <760098065@qq.com>
2020-08-10 22:45:29 +0800

    add reason

--- service/src/main/java/com/melo/left/threadlocal/ThreadId.java
+++ service/src/main/java/com/melo/left/threadlocal/ThreadId.java
@@ -3,6 +3,20 @@ package com.melo.left.threadlocal;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
+ *
+ * 为什么ThreadLocal会产生内存泄漏
+ ThreadLocalMap使用ThreadLocal的弱引用作为key，
+ 如果一个ThreadLocal没有外部强引用来引用它，那么系统 GC 的时候，
+ 这个ThreadLocal势必会被回收，这样一来，
+ ThreadLocalMap中就会出现key为null的Entry，
+ 就没有办法访问这些key为null的Entry的value，如果当前线程再迟迟不结束的话，
+ 这些key为null的Entry的value就会一直存在一条强引用链：
+ Thread Ref -> Thread -> ThreaLocalMap -> Entry -> value
+ 永远无法回收，造成内存泄漏。
+ 其实，ThreadLocalMap的设计中已经考虑到这种情况，
+ 也加上了一些防护措施：在ThreadLocal的get(),set(),remove()
+ 的时候都会清除线程ThreadLocalMap里所有key为null的value。
+
  * ThreadLocal是线程封闭的
  *
  *
