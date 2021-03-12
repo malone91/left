72a0289e655030b0ac391b61375cabe70b80b867
malone91 <760098065@qq.com>
2021-03-12 18:17:32 +0800

    leak

--- service/src/main/java/com/melo/left/MeloApplacation.java
+++ service/src/main/java/com/melo/left/MeloApplacation.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -14,11 +15,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class MeloApplacation {
 
     public static void main(String[] args) {
-        //SpringApplication.run(MeloApplacation.class, args);
+        SpringApplication.run(MeloApplacation.class, args);
     }
 
     @GetMapping("melo")
     public String hello() {
         return "melo";
+    }
+
+
+    @RequestMapping(value = "/test0")
+    public String test0() {
+        ThreadLocal<Byte[]> localVariable = new ThreadLocal<Byte[]>();
+        // 为线程添加变量
+        localVariable.set(new Byte[4096*1024]);
+        return "success";
     }
 }
