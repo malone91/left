dc9099df0bd6741961c85418897c266b694a5ac9
malong5 <malong5@xiaomi.com>
2022-01-21 16:56:15 +0800

    su test

--- service/src/main/java/com/melo/left/training/java/nio/netty/NettyServerApplication.java
+++ service/src/main/java/com/melo/left/training/java/nio/netty/NettyServerApplication.java
@@ -1,5 +1,28 @@
 package com.melo.left.training.java.nio.netty;
 
+/**
+ * sb -u http://localhost:8808/test -c 40 -N 30
+ * wrk -c 40 -d30 http://localhost:8808/test
+ *
+ * 43140   (RPS: 1185.3)                   CreateFile() Error: 5
+ * Status 200:    43041
+ * Status 303:    99
+ *
+ * RPS: 1322.8 (requests/second)
+ * Max: 824ms
+ * Min: 0ms
+ * Avg: 6.1ms
+ *
+ *   50%   below 1ms
+ *   60%   below 2ms
+ *   70%   below 5ms
+ *   80%   below 8ms
+ *   90%   below 14ms
+ *   95%   below 21ms
+ *   98%   below 32ms
+ *   99%   below 44ms
+ * 99.9%   below 263ms
+ */
 public class NettyServerApplication {
 
     public static void main(String[] args) {
