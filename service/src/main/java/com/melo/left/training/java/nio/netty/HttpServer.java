b2761a6a0ef3df5140183ae2c0b4b3015bbee534
malong5 <malong5@xiaomi.com>
2022-01-29 14:54:16 +0800

    netty

--- service/src/main/java/com/melo/left/training/java/nio/netty/HttpServer.java
+++ service/src/main/java/com/melo/left/training/java/nio/netty/HttpServer.java
@@ -13,6 +13,9 @@ import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * bytebuffer.default.direct.heap
+ */
 @Slf4j
 public class HttpServer {
 
@@ -39,16 +42,22 @@ public class HttpServer {
             ServerBootstrap bootstrap = new ServerBootstrap();
             //网络的参数
             bootstrap.option(ChannelOption.SO_BACKLOG, 128)
+                    //Nagle算法开启 能把小包拼成大块
                     .option(ChannelOption.TCP_NODELAY, true)
+                    //长链接
                     .option(ChannelOption.SO_KEEPALIVE, true)
+                    //TIme_wait复用地址和端口
                     .option(ChannelOption.SO_REUSEADDR, true)
-                    //http的接收区和发送区缓冲区大小
+                    //http的接收区和发送区缓冲区大小 32k
                     .option(ChannelOption.SO_RCVBUF, 32 * 1024)
                     .option(ChannelOption.SO_SNDBUF, 32 * 1024)
                     .option(EpollChannelOption.SO_REUSEPORT, true)
+                    //设置在worker上
                     .childOption(ChannelOption.SO_KEEPALIVE, true);
+            //加上NioServerSocketChannel，表示里面都用nio操作
             bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
                     .handler(new LoggingHandler(LogLevel.INFO))
+                    //关键点：代理服务器的参数传入进去 pipeline上加东西
                     .childHandler(new HttpInitializer(sslContext));
             Channel channel = bootstrap.bind(port).sync().channel();
             log.info("启动netty http服务，监听地址和端口为" + (ssl ? "https" : "http") + "://127.0.0.1" + port);
